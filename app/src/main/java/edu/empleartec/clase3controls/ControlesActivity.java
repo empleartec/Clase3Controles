61977997cdd5f1fa002bb6d19e5024be08c6ea69
Gabriel Calero <g@mobaires.com>
2015-11-27 12:00:27 -0300

    Revisión

--- app/src/main/java/edu/empleartec/clase3controls/ControlesActivity.java
+++ app/src/main/java/edu/empleartec/clase3controls/ControlesActivity.java
@@ -1,5 +1,6 @@
 package edu.empleartec.clase3controls;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,7 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
-public class ControlesActivity extends AppCompatActivity {
+public class ControlesActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,41 +20,19 @@ public class ControlesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_controles);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_controles, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up boton_rojo, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void imageButtonClick(View view) {
         EditText editText = (EditText) findViewById(R.id.edit_text);
     }
 
-    public void buttonClick(View view) {
+    public void saludarClick(View view) {
         EditText editText = (EditText) findViewById(R.id.edit_text);
         String userText = editText.getText().toString();
-        Toast.makeText(this, getString(R.string.hola) + userText, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, getString(R.string.hola) + " " +  userText, Toast.LENGTH_SHORT).show();
     }
 
     public void toggleButtonClick(View view) {
         boolean on = ((ToggleButton) view).isChecked();
-
+        Toast.makeText(this, "Toggle button fue  "+(on?" encendido":"apagado"), Toast.LENGTH_SHORT).show();
     }
 
     public void checkboxClick(View view) {
